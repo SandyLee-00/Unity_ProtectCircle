685ee5facb05b36cf701c0953ea6db7034626899
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-10-25 16:55:00 +0900

    feat: shield 마우스 위치에 따라 움직임

--- Assets/Scripts/shield.cs
+++ Assets/Scripts/shield.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class shield : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePos.x, mousePos.y, 0);
     }
 }
